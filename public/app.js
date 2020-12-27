60de30ca17d1e39002b4b61bd732ecb86815a140
Vidhi <vidhi152@gmail.com>
2020-12-28 00:56:40 +0530

    canvas added

--- public/app.js
+++ public/app.js
@@ -16,22 +16,32 @@ const modelParams = {
 handTrack.startVideo(video).then(status => {
     if(status){
         navigator.getUserMedia({video: {}}, (stream) => {
-
+            x = 0; y = 0;
+            context.moveTo(20,50);
             video.srcObject = stream;
-            setInterval(runDetection, 2000);
+            setInterval(runDetection, 1000);
         }, err=> console.log(err));
     }
 });
 
+drawOnCanvas = (x, y) => {
+    context.lineTo(x, y);
+    context.stroke();
+}
+
 runDetection = () => {
     model.detect(video).then(predictions=>{
-        //console.log(predictions);
-        predictions.forEach(prediction => {
-            x = prediction.bbox[0];
-            y = prediction.bbox[1];
-            console.log(x);
-            console.log(y);
-        });
+        console.log(predictions);
+        if(predictions.length > 0){
+            predictions.forEach(prediction => {
+                x = prediction.bbox[0];
+                y = prediction.bbox[1];
+                console.log(x);
+                console.log(y);
+                drawOnCanvas(x, y);
+            });
+        }
+        
        // model.renderPredictions(predictions, canvas, context, video);
     });
 }
